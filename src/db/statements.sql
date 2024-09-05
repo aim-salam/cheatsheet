a298449d2660b0fd1d7a795b47d595e617c2d2c2
Aim Salam <aimsalam.com@gmail.com>
2024-09-05 15:45:58 +0800

    Created first ever .sql file.

---
+++ src/db/statements.sql
@@ -0,0 +1,7 @@
+-- Open database table
+-- file.db > Right mouse > open database
+-- SQLITE EXPLORE ( bawah sekali ) > topic > right mouse > show table
+
+-- Start QUERY
+-- Write > select which text > Right mouse > Run selected query = result on SQLite tab
+SELECT * from topic WHERE topic='html'
