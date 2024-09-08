9dc69eaf21543563f9dec71dbccf617313b05f10
Aim Salam <aimsalam.com@gmail.com>
2024-09-08 10:20:07 +0800

    Implemented CLICell.jsx copy features.

--- src/components/CLICell.jsx
+++ src/components/CLICell.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TableCell, Typography } from "@mui/material";
+import { Stack, TableCell, Typography } from "@mui/material";
 
 function CLICell({ cli }) {
   const textSx = {
@@ -8,11 +8,30 @@ function CLICell({ cli }) {
     padding: 1,
     paddingLeft: 2,
     paddingRight: 2,
-    borderRadius: 2,
   };
   return (
     <TableCell component="th" scope="row">
-      <Typography sx={textSx}> {cli}</Typography>
+      <Stack
+        sx={{
+          position: "relative",
+          borderRadius: "10px",
+          overflow: "hidden",
+          //   position: "absolute",
+          right: 0,
+          top: 0,
+        }}
+      >
+        <Typography
+          color="grey"
+          sx={{
+            backgroundColor: "#111111",
+            width: "100%",
+          }}
+        >
+          Copy
+        </Typography>
+        <Typography sx={textSx}> {cli}</Typography>
+      </Stack>
     </TableCell>
   );
 }
