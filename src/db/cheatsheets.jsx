7cb47762815eebcc4c1566a298dfbb513c47d09f
Aim Salam <aimsalam.com@gmail.com>
2024-09-11 14:34:33 +0800

    Updated format for cheatsheet template.

--- src/db/cheatsheets.jsx
+++ src/db/cheatsheets.jsx
@@ -1,4 +1,4 @@
-const template = {
+const templateCheatsheet = {
   id: "dasdasdada",
   //
   topic: "git",
@@ -6,10 +6,12 @@ const template = {
   //
   action: "Check file changes",
   explanation: "",
+  requirement: [{ text: "", link: "" }],
   //
   visual: [{ text: "", link: "" }],
   gui: "https://images.cheatsheet.cam/images/Screenshot-2024-09-07-at-3.01.22-AM.webp",
   cli: "git ..",
+  code: "",
   //
   emoji: "❄️",
   priority: 1,
