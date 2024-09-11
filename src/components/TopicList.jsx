86540580c47d21f402d86133e495ccae097469b1
Aim Salam <aimsalam.com@gmail.com>
2024-09-11 15:13:32 +0800

    Updated topcis.jsx : Extract topics into topics.jsx

--- src/components/TopicList.jsx
+++ src/components/TopicList.jsx
@@ -8,37 +8,9 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-
+import { topics } from "../db/topics";
 import logo from "./../assets/logo.jpg";
 function TopicList({ setTopic }) {
-  const topics = [
-    {
-      topic: "React",
-      type: "parent",
-      imageLink: "https://images.cheatsheet.cam/images/react.webp",
-    },
-    {
-      topic: "Component",
-      type: "children",
-      imageLink: "https://images.cheatsheet.cam/images/component.webp",
-    },
-    {
-      topic: "State",
-      type: "children",
-      imageLink: "https://images.cheatsheet.cam/images/state.webp",
-    },
-    {
-      topic: "HTML",
-      type: "parent",
-      imageLink: "https://images.cheatsheet.cam/images/html.webp",
-    },
-    {
-      topic: "CSS",
-      type: "parent",
-      imageLink: "https://images.cheatsheet.cam/images/css.webp",
-    },
-  ];
-
   function Header({ topic }) {
     if (topic.type === "parent") {
       return (
---
+++ src/db/topics.jsx
@@ -0,0 +1,27 @@
+export const topics = [
+  //   {
+  //     topic: "React",
+  //     type: "parent",
+  //     imageLink: "https://images.cheatsheet.cam/images/react.webp",
+  //   },
+  //   {
+  //     topic: "Component",
+  //     type: "children",
+  //     imageLink: "https://images.cheatsheet.cam/images/component.webp",
+  //   },
+  //   {
+  //     topic: "State",
+  //     type: "children",
+  //     imageLink: "https://images.cheatsheet.cam/images/state.webp",
+  //   },
+  {
+    topic: "HTML",
+    type: "parent",
+    imageLink: "https://images.cheatsheet.cam/images/html.webp",
+  },
+  //   {
+  //     topic: "CSS",
+  //     type: "parent",
+  //     imageLink: "https://images.cheatsheet.cam/images/css.webp",
+  //   },
+];
