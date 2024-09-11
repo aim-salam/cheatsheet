1c9f2c898f779e264ca29da0b5d4357e7396a3bb
Aim Salam <aimsalam.com@gmail.com>
2024-09-11 19:04:46 +0800

    Updated cheatsheet object template: codeType

--- src/db/cheatsheets.jsx
+++ src/db/cheatsheets.jsx
@@ -12,6 +12,7 @@ const templateCheatsheet = {
   gui: "https://images.cheatsheet.cam/images/Screenshot-2024-09-07-at-3.01.22-AM.webp",
   cli: "git ..",
   code: "",
+  typeCode: "",
   //
   emoji: "❄️",
   priority: 1,
@@ -35,6 +36,19 @@ export const cheatsheets = [
 <a href="./lol.jpg" download>About me</a> //image aut-download
 <a href="#id">About me</a> //jump
 <a href="mailto:xxxxx@gmail.com">About me</a> //email`,
+    typeCode: "html",
+    emoji: "❄️",
+    priority: 1,
+  },
+  {
+    id: "html-hyperlink",
+    topic: "html",
+    table: "element > body childrens element",
+    action: "Title",
+    explanation: "An element with link",
+    visual: "https://images.cheatsheet.cam/images/image-1726039801639.webp",
+    code: `<title>Chat</title>`,
+    typeCode: "html",
     emoji: "❄️",
     priority: 1,
   },
