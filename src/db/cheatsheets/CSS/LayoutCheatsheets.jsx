3f7232e0afe4364cec24ffd01b7964ea9ded758f
Aim Salam <aimsalam.com@gmail.com>
2024-09-13 17:38:01 +0800

    Updated database in LayoutCheatsheet.jsx

--- src/db/cheatsheets/CSS/LayoutCheatsheets.jsx
+++ src/db/cheatsheets/CSS/LayoutCheatsheets.jsx
@@ -17,7 +17,75 @@ const BOX_MODEL = [
         link: "https://images.cheatsheet.cam/images/image-1726218884809.webp",
       },
     ],
-    code: "",
+    code: " ",
+    emoji: "❄️",
+    priority: 1,
+  }),
+  new Cheatsheet({
+    id: "box-mode-border",
+    topic: "Layout",
+    table: "Box model",
+    action: "Border",
+    description: " ",
+    visual: [
+      {
+        text: "Before:",
+        link: "https://images.cheatsheet.cam/images/image-1726219780955.webp",
+      },
+      {
+        text: "After:",
+        link: "https://images.cheatsheet.cam/images/image-1726219793700.webp",
+      },
+    ],
+    code: `border: 5px solid #3435ff; /* width style color */
+border-color:  #3435ff;`,
+    emoji: "❄️",
+    priority: 1,
+  }),
+  new Cheatsheet({
+    id: "box-mode-padding",
+    topic: "Layout",
+    table: "Box model",
+    action: "Padding",
+    description: " ",
+    visual: [
+      {
+        text: "Before:",
+        link: "https://images.cheatsheet.cam/images/image-1726219793700.webp",
+      },
+      {
+        text: "After:",
+        link: "https://images.cheatsheet.cam/images/image-1726219983411.webp",
+      },
+    ],
+    code: `padding :10px; /* all side */
+padding: 10px 20px;  /* vertical horizontal */
+padding: 10px 20px 30px 40px; /* top right bottom left */
+
+
+padding-left: 10px;/* only left */
+padding-right: 10px;/* only right */
+padding-top: 10px;/* only top */`,
+    emoji: "❄️",
+    priority: 1,
+  }),
+  new Cheatsheet({
+    id: "box-mode-margin",
+    topic: "Layout",
+    table: "Box model",
+    action: "Margin",
+    description: " ",
+    visual: [
+      {
+        text: "Before:",
+        link: "https://images.cheatsheet.cam/images/image-1726220205859.webp",
+      },
+      {
+        text: "After:",
+        link: "https://images.cheatsheet.cam/images/image-1726220218776.webp",
+      },
+    ],
+    code: `/* same like padding */`,
     emoji: "❄️",
     priority: 1,
   }),
