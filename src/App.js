04ed94c3700e83d4395faa3e66f72ccb081e677d
Aim Salam <aimsalam.com@gmail.com>
2024-09-15 12:39:47 +0800

    Update table cell

--- src/App.js
+++ src/App.js
@@ -37,10 +37,66 @@ const analytics = getAnalytics(app);
 function App() {
   const { mode } = useColorScheme();
   const [topic, setTopic] = useState({
-    topic: "CSS",
-    type: "parent",
-    imageLink: "https://images.cheatsheet.cam/images/css.webp",
-    tables: [],
+    topic: "Layout",
+    type: "children",
+    imageLink: "https://images.cheatsheet.cam/images/image-1726097807593.webp",
+    tables: [
+      {
+        table: "Box model",
+        columns: {
+          action: true,
+          description: true,
+          visual: true,
+          code: true,
+          attention: true,
+          // emoji: true,
+        },
+      },
+      {
+        table: "Overflow",
+        columns: {
+          action: true,
+          description: true,
+          visual: true,
+          code: true,
+          attention: true,
+          // emoji: true,
+        },
+      },
+      {
+        table: "Measurement Unit",
+        columns: {
+          action: true,
+          description: true,
+          visual: true,
+          code: true,
+          attention: true,
+          // emoji: true,
+        },
+      },
+      {
+        table: "Positioning",
+        columns: {
+          action: true,
+          description: true,
+          visual: true,
+          code: true,
+          attention: true,
+          // emoji: true,
+        },
+      },
+      {
+        table: "Flexbox",
+        columns: {
+          action: true,
+          description: true,
+          visual: true,
+          code: true,
+          attention: true,
+          // emoji: true,
+        },
+      },
+    ],
   });
 
   useEffect(() => {
