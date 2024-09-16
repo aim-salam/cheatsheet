7d9bde4caa7a02acf1e61e4df23cb7b4f731d7ce
Aim Salam <aimsalam.com@gmail.com>
2024-09-16 16:04:24 +0800

    Updated TopicDataCheatsheet.jsx database.

--- src/db/cheatsheets.jsx
+++ src/db/cheatsheets.jsx
@@ -2,9 +2,11 @@ import { HTMLCheatsheet } from "./cheatsheets/HTML/HTMLCheatsheets";
 import { LayoutCheatsheets } from "./cheatsheets/CSS/LayoutCheatsheets";
 import { ReactCheatsheet } from "./cheatsheets/React/ReactCheatsheet";
 import { MemoryCheatsheet } from "./cheatsheets/React/MemoryCheatsheet";
+import { PassDataCheatsheet } from "./cheatsheets/React/PassDataCheatsheet";
 export const cheatsheets = [
   ...HTMLCheatsheet,
   ...LayoutCheatsheets,
   ...ReactCheatsheet,
   ...MemoryCheatsheet,
+  ...PassDataCheatsheet,
 ];
