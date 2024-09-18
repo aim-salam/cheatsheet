d9ca1391c6f387e6ea1ecdc21ffd05e17afbbe39
Aim Salam <aimsalam.com@gmail.com>
2024-09-18 14:52:24 +0800

    Refactored Aside.jsx

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {
   List,
   ListItemButton,
-  ListItemText,
   ListItemAvatar,
   Avatar,
   Stack,
@@ -12,77 +11,73 @@ import {
 import { topics } from "../db/topics";
 import logo from "./../assets/logo.jpg";
 function TopicList({ setTopic }) {
-  function Header({ topic }) {
-    if (topic.type === "parent") {
-      return (
-        <Typography
-          marginTop={"20px"}
-          paddingLeft={"40px"}
-          paddingRight={"20px"}
-          fontSize={"18px"}
-          fontWeight={"bold"}
-        >
-          {topic.type === "parent" ? `${topic.topic}` : null}
-        </Typography>
-      );
-    }
-  }
+  const parentStyle = {
+    marginTop: "40px",
+    marginBottom: "10px",
+    fontSize: "25px",
+    avatarSize: "30px",
+  };
+
+  const childStyle = {
+    fontSize: "17px",
+    avatarSize: "25px",
+  };
+
+  const getStyle = (type) => (type === "parent" ? parentStyle : childStyle);
 
   return (
     <Grid2
-      // item
+      item
       xs={12}
       sm={3}
       md={2}
       sx={{ display: { xs: "none", sm: "block" }, width: "250px" }}
     >
       <List>
-        {topics.map((topic) => (
-          <Stack
-            key={topic.topic}
-            marginTop={topic.type === "parent" ? "40px" : null}
-            marginBottom={topic.type === "parent" ? "10px" : null}
-            height={"40px"}
-          >
-            {/* <Header topic={topic}></Header> */}
-            <ListItemButton
-              sx={{
-                paddingLeft: "40px",
-                paddingRight: "30px",
-                paddingTop: "1px",
-                paddingBottom: "1px",
-              }}
-              onClick={() => {
-                setTopic(topic);
-              }}
-            >
-              <ListItemAvatar>
-                <Avatar
-                  sx={{
-                    width: topic.type === "parent" ? "30px" : "25px",
-                    height: topic.type === "parent" ? "30px" : "25px",
-                  }}
-                  alt="Remy Sharp"
-                  src={topic.imageLink ? topic.imageLink : logo}
-                  variant="rounded"
-                />
-              </ListItemAvatar>
+        {topics.map((topic) => {
+          const { topic: topicName, type, imageLink } = topic;
+          const { marginTop, marginBottom, fontSize, avatarSize } =
+            getStyle(type);
 
-              <Typography
-                fontWeight={topic.type === "grandchildren" ? null : "bold"}
-                fontSize={topic.type === "parent" ? "25px" : "17px"}
+          return (
+            <Stack
+              key={topicName}
+              marginTop={marginTop}
+              marginBottom={marginBottom}
+              height={"40px"}
+            >
+              <ListItemButton
+                sx={{
+                  paddingLeft: "40px",
+                  paddingRight: "30px",
+                  paddingTop: "1px",
+                  paddingBottom: "1px",
+                }}
+                onClick={() => {
+                  setTopic(topic);
+                }}
               >
-                {topic.topic}
-              </Typography>
-            </ListItemButton>
-          </Stack>
-        ))}
+                <ListItemAvatar>
+                  <Avatar
+                    sx={{ width: avatarSize, height: avatarSize }}
+                    alt={topicName}
+                    src={imageLink || logo}
+                    variant="rounded"
+                  />
+                </ListItemAvatar>
+
+                <Typography
+                  fontWeight={type !== "grandchildren" ? "bold" : "normal"}
+                  fontSize={fontSize}
+                >
+                  {topic.topic}
+                </Typography>
+              </ListItemButton>
+            </Stack>
+          );
+        })}
       </List>
     </Grid2>
-
-    // <ul>
-
-    // </ul>
   );
 }
 
