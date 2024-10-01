da83f2fc50c05fa8af31809dfb9d88816662267b
Aim Salam <aimsalam.com@gmail.com>
2024-10-01 08:34:27 +0800

    Fixed UniversalCell.jsx align.

--- src/components/TableCells/UniversalCell.jsx
+++ src/components/TableCells/UniversalCell.jsx
@@ -4,69 +4,65 @@ import VisualModal from "../Modals/VisualModal";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import {
   vscDarkPlus,
-  vs,
   prism,
 } from "react-syntax-highlighter/dist/esm/styles/prism";
-function UniversalCell({ data, customTableCellStyle, customFont }) {
+
+function UniversalCell({ data, width }) {
   const [modalUrl, setModalUrl] = useState("");
+
   const customCodeStyle = {
-    // fontSize: customFont,
     marginTop: "0px",
     borderRadius: "10px",
     fontFamily:
       'Menlo, Monaco, Consolas, "Andale Mono", "Ubuntu Mono", "Courier New", monospace',
   };
 
+  const handleImageClick = (url) => setModalUrl(url);
+  const handleCloseModal = () => setModalUrl("");
+
   return (
-    <TableCell sx={customTableCellStyle}>
+    <TableCell sx={{ alignContent: "flex-start", width: width }}>
       <div>
-        {data.map((item) => {
-          return (
-            <Stack key={data}>
-              {item.text ? (
-                <Typography fontSize={customFont}>{item.text}</Typography>
-              ) : null}
-              {item.image_link ? (
-                <>
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      width: "200px",
-                      objectFit: "cover",
-                      "&:hover": {
-                        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)", // Shadow effect on hover
-                      },
-                    }}
-                    onClick={() => {
-                      setModalUrl(item.image_link);
-                    }}
-                    image={item.image_link}
-                    alt="Visual image"
-                  />
-                  <VisualModal
-                    visual={modalUrl}
-                    open={modalUrl !== "" ? true : false}
-                    handleClose={() => {
-                      setModalUrl("");
-                    }}
-                  ></VisualModal>
-                </>
-              ) : null}
+        {data.map((item, index) => (
+          <Stack key={index}>
+            {item.text && (
+              <Typography fontSize={"17px"}>{item.text}</Typography>
+            )}
 
-              {item.code ? (
-                <SyntaxHighlighter
-                  language={item.code_type}
-                  // style={mode === "dark" ? vscDarkPlus : prism}
-                  customStyle={customCodeStyle}
-                >
-                  {item.code}
-                </SyntaxHighlighter>
-              ) : null}
+            {item.image_link && (
+              <>
+                <CardMedia
+                  component="img"
+                  sx={{
+                    width: "200px",
+                    objectFit: "cover",
+                    "&:hover": {
+                      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.3)", // Shadow effect on hover
+                    },
+                  }}
+                  onClick={() => handleImageClick(item.image_link)}
+                  image={item.image_link}
+                  alt="Visual image"
+                />
+                <VisualModal
+                  visual={modalUrl}
+                  open={modalUrl !== ""}
+                  handleClose={handleCloseModal}
+                />
+              </>
+            )}
 
-              <br />
-            </Stack>
-          );
-        })}
+            {item.code && (
+              <SyntaxHighlighter
+                language={item.code_type}
+                style={vscDarkPlus} // You can switch between dark and light themes here
+                customStyle={customCodeStyle}
+              >
+                {item.code}
+              </SyntaxHighlighter>
+            )}
+          </Stack>
+        ))}
       </div>
     </TableCell>
   );
--- src/components/Tables/TopicRow.jsx
+++ src/components/Tables/TopicRow.jsx
@@ -1,40 +1,21 @@
 import React from "react";
-import { TableRow, TableCell } from "@mui/material";
-import ActionCell from "../TableCells/ActionCell";
-import VisualCell from "../TableCells/VisualCell";
-import GUICell from "../TableCells/GUICell";
-import CLICell from "../TableCells/CLICell";
-import CodeCell from "../TableCells/CodeCell";
-import EmojiCell from "../TableCells/EmojiCell";
+import { TableRow } from "@mui/material";
 import OptionsCell from "../TableCells/OptionsCell";
-import DescriptionCell from "../TableCells/DescriptionCell";
-import AttentionCell from "../TableCells/AttentionCell";
 import UniversalCell from "../TableCells/UniversalCell";
-function TopicRow({ row, rows, isEditable, setRows }) {
-  const customTableCellStyle = {
-    borderRight: "1px solid #EDEDED",
-  };
-  const customFont = "17px";
 
-  const keyData = [
-    { key: "action", value: row.item.item.action },
-    {
-      key: "description",
-      value: row.item.item.description,
-    },
-    { key: "visual", value: row.item.item.visual },
-    // { key: "gui", component: GUICell, value: row.item.item.gui },
-    // { key: "cli", component: CLICell, value: row.item.item.cli },
-    {
-      key: "code",
-      value: row.item.item.code,
-    },
-    // {
-    //   key: "attention",
-    //   component: AttentionCell,
-    //   value: row.item.item.attention,
-    // },
-    // { key: "emoji", component: EmojiCell, value: row.item.item.emoji },
+function TopicRow({ row, rows, isEditable, setRows }) {
+  const { action, description, visual, gui, cli, code, attention, emoji } =
+    row.item.item;
+
+  const rowData = [
+    { key: "action", value: action, width: "100px" },
+    { key: "description", value: description, width: "100px" },
+    { key: "visual", value: visual, width: "100px" },
+    { key: "gui", value: gui, width: "100px" },
+    { key: "cli", value: cli, width: "100px" },
+    { key: "code", value: code, width: "100px" },
+    { key: "attention", value: attention, width: "100px" },
+    { key: "emoji", value: emoji, width: "100px" },
   ];
 
   return (
@@ -44,29 +25,17 @@ function TopicRow({ row, rows, isEditable, setRows }) {
         borderBottom: "1px solid #EDEDED",
       }}
     >
-      {/* New Design: many cell will return UniversalCell */}
-      {/* if row have keys represent columns is true, map will UniversalCell  */}
-      {keyData.map(
-        ({ key, value, extraProps = {} }) =>
-          value && (
-            <UniversalCell
-              key={value}
-              data={value}
-              customTableCellStyle={customTableCellStyle}
-              customFont={customFont}
-            ></UniversalCell>
-          )
+      {rowData.map(({ key, value, width }) =>
+        value ? <UniversalCell key={key} data={value} width={width} /> : null
       )}
 
-      {/* Render options cell if isOptions is true */}
+      {/* Render OptionsCell if isEditable is true */}
       {isEditable && (
         <OptionsCell
-          key={row.item.item.action + "isOption"}
+          key={`${action}-options`}
           row={row}
           rows={rows}
           setRows={setRows}
-          customTableCellStyle={customTableCellStyle}
-          customFont={customFont}
         />
       )}
     </TableRow>
