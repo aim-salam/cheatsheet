f6c524bc8d21854a35eb3622b157202654491917
Aim Salam <aimsalam.com@gmail.com>
2024-10-10 15:25:37 +0800

    Reformat route format.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-import HomePage from "./pages/HomePage";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+
 function App() {
-  return <HomePage></HomePage>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
