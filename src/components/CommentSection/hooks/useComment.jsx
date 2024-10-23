5f77c376a2f94c84450f71fce20d6a6b6af7819b
Aim Salam <aimsalam.com@gmail.com>
2024-10-23 18:28:46 +0800

    Refactor code : useComment.jsx

--- src/components/CommentSection/hooks/useComment.jsx
+++ src/components/CommentSection/hooks/useComment.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { useTopic } from "../../../contexts/TopicContext";
@@ -11,179 +10,156 @@ import {
   getDownloadURL,
   deleteObject,
 } from "firebase/storage";
+
+const COMMENT_URL = import.meta.env.VITE_COMMENT_URL;
+
 function useBooking() {
   const [bookings, setBookings] = useState([]);
-  const [receiver_email, setReceiver] = useState("");
+  const [receiverEmail, setReceiverEmail] = useState("");
   const [comment, setComment] = useState("");
-
   const [editingIndex, setEditingIndex] = useState(null);
-
   const [image, setImage] = useState(null);
-  const [preview, setPreview] = useState(null);
   const [progress, setProgress] = useState(0);
   const [uploading, setUploading] = useState(false);
   const [imageURL, setImageURL] = useState("");
 
-  const { topic, setTopic } = useTopic();
+  const { topic } = useTopic();
   const { user } = useAuth();
 
-  const handleUploadToFirebase = () => {
-    if (image) {
-      const storageRef = ref(storage, `comment_images/${image.name}`);
-      const uploadTask = uploadBytesResumable(storageRef, image);
-
-      setUploading(true);
-
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progressPercent =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          setProgress(progressPercent);
-        },
-        (error) => {
-          console.error("Upload failed:", error);
-          setUploading(false);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            setImageURL(downloadURL);
-            setImage(null);
-            setPreview(null);
-            setUploading(false);
-            setProgress(0);
-            handleUploadToFirestore(downloadURL);
-          });
-        }
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch(
+        `${COMMENT_URL}/comment/topic/${topic.topic}`
       );
-    } else {
-      handleUploadToFirestore(imageURL);
+      const data = await response.json();
+      setBookings(data);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
     }
   };
 
-  const handleUploadToFirestore = (image_url) => {
-    console.log(1, image_url);
-    if (comment.trim()) {
-      if (editingIndex !== null) {
-        const updatedBookings = [...bookings];
-        const updatedData = {
-          ...updatedBookings[editingIndex],
-          comment,
-          receiver_email,
-          image_url,
-        };
-        updatedBookings[editingIndex] = updatedData;
-        setBookings(updatedBookings);
-        axios
-          .put(
-            `${import.meta.env.VITE_COMMENT_URL}/comment/${
-              updatedBookings[editingIndex].id
-            }`,
-            updatedData
-          )
-          .then((response) => {
-            console.log("Success:", response.data);
-          })
-          .catch((error) => {
-            console.error("Error", error);
-          });
-        setEditingIndex(null);
-      } else {
-        const newData = {
-          id: uuidv4(),
-          comment,
-          receiver_email,
-          date: new Date().toISOString(),
-          topic: topic.topic,
-          image_url: image_url,
-          //random
-          sender_email: user.email,
-          profile_url: user.photoURL || "",
-        };
-        console.log(topic);
+  const handleUploadToFirebase = () => {
+    if (!image && !imageURL) return handleUploadToFirestore(imageURL);
 
-        setBookings([...bookings, newData]);
+    const storageRef = ref(storage, `comment_images/${image.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, image);
 
-        axios
-          .post(`${import.meta.env.VITE_COMMENT_URL}/comment`, newData)
-          .then((response) => {
-            console.log("Success:", response.data);
-          })
-          .catch((error) => {
-            console.error("Error", error);
-          });
+    setUploading(true);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progressPercent =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progressPercent);
+      },
+      (error) => {
+        console.error("Upload failed:", error);
+        setUploading(false);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          handleUploadToFirestore(downloadURL);
+        });
       }
-      setComment("");
-      setReceiver("");
-      setImageURL("");
+    );
+  };
+
+  const handleUploadToFirestore = async (image_url) => {
+    if (!comment.trim()) return;
+
+    const updatedBookings = [...bookings];
+    const newData = {
+      id: uuidv4(),
+      comment,
+      receiver_email: receiverEmail,
+      date: new Date().toISOString(),
+      topic: topic.topic,
+      image_url,
+      sender_email: user.email,
+      profile_url: user.photoURL || "",
+    };
+
+    if (editingIndex !== null) {
+      updatedBookings[editingIndex] = {
+        ...updatedBookings[editingIndex],
+        ...newData,
+      };
+      setBookings(updatedBookings);
+
+      try {
+        await axios.put(
+          `${COMMENT_URL}/comment/${updatedBookings[editingIndex].id}`,
+          updatedBookings[editingIndex]
+        );
+      } catch (error) {
+        console.error("Error updating booking:", error);
+      } finally {
+        setEditingIndex(null);
+      }
+    } else {
+      setBookings((prev) => [...prev, newData]);
+      try {
+        await axios.post(`${COMMENT_URL}/comment`, newData);
+      } catch (error) {
+        console.error("Error creating booking:", error);
+      }
     }
-  };
 
-  const handleBookingSubmit = () => {
-    handleUploadToFirebase();
-  };
-
-  const handleEditBooking = (index) => {
-    console.log(bookings[index]);
-    setComment(bookings[index].comment);
-    setReceiver(bookings[index].receiver_email);
-    setImageURL(bookings[index].image_url);
-    setEditingIndex(index);
+    // Clear input fields after submission
+    resetForm();
   };
 
   const handleDeleteBooking = async (index) => {
-    const deletedBookings = bookings[index];
+    const deletedBooking = bookings[index];
     const updatedBookings = bookings.filter((_, i) => i !== index);
     setBookings(updatedBookings);
 
-    axios
-      .delete(
-        `${import.meta.env.VITE_COMMENT_URL}/comment/${bookings[index].id}`
-      )
-      .then((response) => {
-        const fileRef = ref(storage, `${deletedBookings.image_url}`);
-        deleteObject(fileRef);
-
-        console.log("Success:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error", error);
-      });
+    try {
+      await axios.delete(`${COMMENT_URL}/comment/${deletedBooking.id}`);
+      const fileRef = ref(storage, `${deletedBooking.image_url}`);
+      await deleteObject(fileRef);
+    } catch (error) {
+      console.error("Error deleting booking:", error);
+    }
   };
 
-  const fetchPosts = () => {
-    console.log("hello");
-    fetch(`${import.meta.env.VITE_COMMENT_URL}/comment/topic/${topic.topic}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setBookings([...data]);
-        console.log(data);
-      })
-      .catch((error) => console.error("Error:", error));
+  const handleEditBooking = (index) => {
+    const bookingToEdit = bookings[index];
+    setComment(bookingToEdit.comment);
+    setReceiverEmail(bookingToEdit.receiver_email);
+    setImageURL(bookingToEdit.image_url);
+    setEditingIndex(index);
+  };
+
+  const resetForm = () => {
+    setComment("");
+    setReceiverEmail("");
+    setImageURL("");
+    setImage(null);
+    setUploading(false);
+    setProgress(0);
   };
 
   useEffect(() => {
     fetchPosts();
-    console.log(topic);
   }, [topic]);
 
   return {
     bookings,
     setBookings,
-    receiver_email,
-    setReceiver,
+    receiverEmail,
+    setReceiverEmail,
     comment,
     setComment,
     editingIndex,
     setEditingIndex,
-    handleBookingSubmit,
+    handleBookingSubmit: handleUploadToFirebase,
     handleEditBooking,
     handleDeleteBooking,
     progress,
     uploading,
     setImage,
-    preview,
-    setPreview,
   };
 }
 
